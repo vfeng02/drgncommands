63c5960bd3dda488a447fd6320d12da682908f81
Vicky Feng <vickyfeng@vpn12-client-172-20-222-28.princeton.edu>
2024-01-04 14:52:06 +1100

    fix rendering of command descriptions

--- src/renderer/src/components/CommandsList.jsx
+++ src/renderer/src/components/CommandsList.jsx
@@ -7,7 +7,7 @@ const CommandList = ({ commands }) => {
   return (
     <div className="command-list">
       {Object.entries(commands).map(([command_name,command_info]) => (
-        <div className="command-preview" key={"summary_"+command_name} >
+        <div className="command-preview" key={"summary_"+ command_name} >
         <Link to={"/generate"} key={"link"+command_name} state={{fromCommand: [command_name, command_info.args]}}>
             <h2>{ command_name }</h2>
             <p>{ command_info.desc }</p>
--- src/renderer/src/pages/Home.jsx
+++ src/renderer/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import data from '../db.json';
+import commands from '../db.json';
 import { useState } from "react";
 import CommandsList from "../components/CommandsList";
 import Loading from "../components/Loading";
@@ -8,12 +8,13 @@ import useFetch from "../useFetch";
 
 const Home = () => {
   // const { error, isPending, data: commands } = useFetch('http://localhost:8000/commands');
+  console.log(commands["direct_traversal"].desc);
 
   return (
     <div className="home">
       {/* { error && <Error errorMessage="Unable to load data" /> }
       { isPending && <Loading />} */}
-      { data && <CommandsList commands={data.commands} /> }
+      { commands && <CommandsList commands={commands} /> }
     </div>
   );
 }
